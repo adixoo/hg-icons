0801fc7d5e791015484b1afdb68652dcbff5f628
Aditya Kumar <124980341+adixoo@users.noreply.github.com>
2024-11-03 12:28:22 +0530

    added icons

--- stroke/index.tsx
+++ stroke/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { type SVGProps } from "react";
 
 export const Mail01Icon = (props: React.SVGProps<SVGSVGElement>) => (
    <svg
@@ -350,6 +350,37 @@ export const Home09Icon = (props: React.SVGProps<SVGSVGElement>) => (
          stroke="currentColor"
          strokeWidth="1.5"
          strokeLinejoin="round"
+      />
+   </svg>
+);
+
+export const DotIcon = (props: React.SVGProps<SVGSVGElement>) => (
+   <svg
+      fill="currentColor"
+      width={24}
+      height={24}
+      viewBox="0 0 16 16"
+      {...props}
+   >
+      <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
+   </svg>
+);
+
+export const ThreadsIcon = (props: React.SVGProps<SVGSVGElement>) => (
+   <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={24}
+      height={24}
+      color={"currentColor"}
+      fill={"none"}
+      {...props}
+   >
+      <path
+         d="M19.25 8.50488C17.6729 2.63804 12.25 3.00452 12.25 3.00452C12.25 3.00452 4.75 2.50512 4.75 12C4.75 21.4949 12.25 20.9955 12.25 20.9955C12.25 20.9955 16.7077 21.2924 18.75 17.0782C19.4167 15.2204 19.25 11.5049 12.75 11.5049C12.75 11.5049 9.75 11.5049 9.75 14.0049C9.75 14.9812 10.75 16.0049 12.25 16.0049C13.75 16.0049 15.4212 14.9777 15.75 13.0049C16.75 7.00488 11.25 6.50488 9.75 9.00488"
+         stroke="currentColor"
+         strokeWidth="1.5"
+         strokeLinecap="round"
       />
    </svg>
 );
